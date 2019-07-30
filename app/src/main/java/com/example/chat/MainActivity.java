3c5fa9fbcd0acb728afee6f366a558f12bdeac4c
Debanshu777 <debanshudatta123@gmail.com>
2019-07-30 21:38:49 +0530

    fourth commit

--- app/src/main/java/com/example/chat/MainActivity.java
+++ app/src/main/java/com/example/chat/MainActivity.java
@@ -10,19 +10,94 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 
+import com.bumptech.glide.Glide;
 import com.example.chat.Fragment.AddFragment;
 import com.example.chat.Fragment.HomeFragment;
 import com.example.chat.Fragment.NotificatonFragment;
+import com.example.chat.Fragment.SearchFragment;
+import com.example.chat.Model.User;
 import com.google.android.material.bottomappbar.BottomAppBar;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import de.hdodenhof.circleimageview.CircleImageView;
 
 public class MainActivity extends AppCompatActivity {
 
+    CircleImageView profile_pic;
+    FloatingActionButton fab1,fab2,fab3,fab4,fab5;
+    Fragment selctedfragment=null;
+    FirebaseUser firebaseuser;
+    DatabaseReference reference;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        profile_pic=findViewById(R.id.profile_pic);
+        fab1=findViewById(R.id.fab1);
+        fab2=findViewById(R.id.fab2);
+        fab3=findViewById(R.id.fab3);
+        fab4=findViewById(R.id.fab4);
+        fab5=findViewById(R.id.fab5);
+        final Fragment[] selectedFragment = {null};
+        fab1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                selectedFragment[0] =new HomeFragment();
+            }
+        });
+        fab2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                selectedFragment[0] =new SearchFragment();
+            }
+        });
+        fab3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                selectedFragment[0] =new AddFragment();
+            }
+        });
+        fab4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                selectedFragment[0] =new NotificatonFragment();
+            }
+        });
+        fab5.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+        firebaseuser=FirebaseAuth.getInstance().getCurrentUser();
+        reference= FirebaseDatabase.getInstance().getReference().child(firebaseuser.getUid());
+        reference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                User user=dataSnapshot.getValue(User.class);
+                if(user.getImageURL().equals("default"))
+                {
+                    profile_pic.setImageResource(R.drawable.ic_person);
+                }
+                else
+                {
+                    Glide.with(MainActivity.this).load(user.getImageURL()).into(profile_pic);
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
 
 
 
